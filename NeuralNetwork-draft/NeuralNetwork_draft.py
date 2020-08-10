78e5d7970e9e6584ce1e74fa6ffc7fa34d8f69aa
Apricott <krol.michal99@gmail.com>
2020-08-10 20:07:28 +0200

    cost function added

--- NeuralNetwork-draft/NeuralNetwork_draft.py
+++ NeuralNetwork-draft/NeuralNetwork_draft.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.optimize import fmin_ncg
+from activation import sigmoid
 
 def lrCostFunction(theta, X, y, lmbd, activ_func):
 
@@ -22,7 +24,7 @@ def lrCostFunction(theta, X, y, lmbd, activ_func):
     return J, grad
 
 
-def predict(Theta: list, X: np.ndarray, activation: object) -> np.ndarray:
+def predict(Theta: list, X: np.ndarray, activation: object = sigmoid) -> np.ndarray:
     """
     Predict output of NN classifier given the list of parameters Theta, values array X and activation function used to train NN classifier
     
@@ -65,12 +67,59 @@ def predict(Theta: list, X: np.ndarray, activation: object) -> np.ndarray:
     return p
 
 
-def nnCostFunction(nn_params, layer_sizes, num_classes, X, y, lmbd):
+def nnCostFunction(nn_params: np.ndarray, layer_sizes: list, num_classes: int, X: np.ndarray, y: np.ndarray, lmbd: float = 0, activation: object = sigmoid) -> float:
     """
+    Return cost value for given weights array nn_params and values array X with assigned classes in array y
+
+    nn_params   - 1 dimensional array of weights associated with every neuron in neural network.
+                  Flattening of weights arrays is necessary to correctly feed them to the optimization function.
+
+    layer_sizes - list with sizes of every layer in network, including the input and output layers,
+                  WITHOUT the bias units. 
+
+    num_classes - integer specifying the number of existig classes
+
+    lmbd        - regularization parameter, omits the bias units
+    
+    activation  - activation function used to train NN classifier
 
     """
+
+    # Initialize some useful variables
     J = 0
-    grad = 0
+    J_reg = 0
+    m = X.shape[0]
+    p = np.zeros((m, 1))
 
+    y = np.eye(num_classes)[y,:].reshape((m, num_classes))
 
-    return J, grad
+    Theta = []
+    split = 0
+
+    # reshape Theta arrays from nn_params array back to their original shape
+    for i in range(0, len(layer_sizes) - 1):
+        temp = layer_sizes[i + 1] * (layer_sizes[i] + 1) + split
+        size = (layer_sizes[i + 1], layer_sizes[i] + 1)
+        Theta += [np.reshape(nn_params[split:temp], size)]
+        split = temp
+    
+    # Add bias unit to the X array
+    a = np.concatenate([np.ones((m, 1)), X], axis=1)
+
+    # Feedforward the neural network
+    for layer, theta in enumerate(Theta, 1):
+
+        # calculate result of activating layer
+        a = activation(a @ theta.T)    
+        J_reg = J_reg + np.sum(np.sum((theta[:, 1:] ** 2)))
+
+        #  Add bias unit to the resulting array if it's not the last layer
+        if layer != len(Theta):
+            a = np.concatenate([np.ones((a.shape[0], 1)), a], axis=1)
+
+    J_reg *= lmbd/(2*m)
+    J = np.sum(np.sum((-y * np.log(a)) - ((1 - y) * np.log(1 - a)))) / m
+    J += J_reg
+
+    return J
+
--- NeuralNetwork-draft/testing/NN-test.py
+++ NeuralNetwork-draft/testing/NN-test.py
@@ -1,10 +1,12 @@
 import unittest 
-import sys
-import os
+import sys, os
 import numpy as np
 import pandas as pd
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
+
+#print(os.getcwd())
+
 import activation 
 import NeuralNetwork_draft
 
@@ -34,11 +36,11 @@ class TestActivationFunctions(unittest.TestCase):
 class TestNNPrediction(unittest.TestCase):
 
     def setUp(self):
-        self.X = pd.read_csv('NeuralNetwork-draft/testing/X.csv', header=None).to_numpy()
+        self.X = pd.read_csv('testing/X.csv', header=None).to_numpy()
         ## classes in this csv are indexed from 1 to 10 rather than 0 to 9, hence subtraction of 1
-        self.y = (pd.read_csv('NeuralNetwork-draft/testing/y.csv', header=None).to_numpy() - 1).T
-        self.Theta1 = pd.read_csv('NeuralNetwork-draft/testing/Theta1.csv', header=None).to_numpy()
-        self.Theta2 = pd.read_csv('NeuralNetwork-draft/testing/Theta2.csv', header=None).to_numpy()
+        self.y = (pd.read_csv('testing/y.csv', header=None).to_numpy() - 1).T
+        self.Theta1 = pd.read_csv('testing/Theta1.csv', header=None).to_numpy()
+        self.Theta2 = pd.read_csv('testing/Theta2.csv', header=None).to_numpy()
         self.pred = NeuralNetwork_draft.predict([self.Theta1, self.Theta2], self.X, activation.sigmoid)
 
     def test_predictions(self):
@@ -48,23 +50,23 @@ class TestNNPrediction(unittest.TestCase):
 class TestNNCostFunction(unittest.TestCase):
 
     def setUp(self):
-        self.X = pd.read_csv('NeuralNetwork-draft/testing/X.csv', header=None).to_numpy()
+        self.X = pd.read_csv('testing/X.csv', header=None).to_numpy()
         ## classes in this csv are indexed from 1 to 10 rather than 0 to 9, hence subtraction of 1
-        self.y = (pd.read_csv('NeuralNetwork-draft/testing/y.csv', header=None).to_numpy() - 1).T
-        self.Theta1 = pd.read_csv('NeuralNetwork-draft/testing/Theta1.csv', header=None).to_numpy()
-        self.Theta2 = pd.read_csv('NeuralNetwork-draft/testing/Theta2.csv', header=None).to_numpy()
-        self.nn_params = [0, 0]     # <- popraw! maja byc parametry do fmincg
-        self.layer_sizes = [400, 25]
+        self.y = pd.read_csv('testing/y.csv', header=None).to_numpy() - 1
+        self.Theta1 = pd.read_csv('testing/Theta1.csv', header=None).to_numpy()
+        self.Theta2 = pd.read_csv('testing/Theta2.csv', header=None).to_numpy()
+        self.nn_params = np.concatenate([np.reshape(self.Theta1, (25 * 401, 1)), np.reshape(self.Theta2, (10 * 26, 1))])
+        self.layer_sizes = [400, 25, 10]
         self.num_classes = 10
         self.lmbd = 0
 
     def test_cost(self):
-        J, _ = NeuralNetwork_draft.nnCostFunction(self.nn_params, self.layer_sizes, self.num_classes, self.X, self.y, self.lmbd)
+        J = NeuralNetwork_draft.nnCostFunction(self.nn_params, self.layer_sizes, self.num_classes, self.X, self.y, self.lmbd, activation.sigmoid)
         self.assertAlmostEqual(J, 0.287629, 3, "Wrong cost!")
 
     def test_costWithRegularization(self):
         self.lmbd = 1
-        J, _ = NeuralNetwork_draft.nnCostFunction(self.nn_params, self.layer_sizes, self.num_classes, self.X, self.y, self.lmbd)
+        J = NeuralNetwork_draft.nnCostFunction(self.nn_params, self.layer_sizes, self.num_classes, self.X, self.y, self.lmbd, activation.sigmoid)
 
 if __name__ == '__main__':
     unittest.main()
